c8116902019a4cfa71ab10c647781ea25ed08508
AbsHp <asinghal21@iitk.ac.in>
2024-11-28 19:14:15 +0530

    try4

--- Congrats Animation/app.js
+++ Congrats Animation/app.js
@@ -1,67 +1,63 @@
 $(function() {
-    var numberOfStars = 20;
-    
-    for (var i = 0; i < numberOfStars; i++) {
-      $('.congrats').append('<div class="blob fa fa-star ' + i + '"></div>');
-    }    
+    const numberOfStars = 20;
+
+    for (let i = 0; i < numberOfStars; i++) {
+        $('.congrats').append(`<div class="blob fa fa-star ${i}"></div>`);
+    }
 
     animateText();
-    
     animateBlobs();
 });
 
 $('.congrats').click(function() {
     reset();
-    
     animateText();
-    
     animateBlobs();
 });
 
 function reset() {
-    $.each($('.blob'), function(i) {
-        TweenMax.set($(this), { x: 0, y: 0, opacity: 1 });
+    $('.blob').each(function() {
+        gsap.set($(this), { x: 0, y: 0, opacity: 1 });
     });
-    
-    TweenMax.set($('h1'), { scale: 1, opacity: 1, rotation: 0 });
+
+    gsap.set($('h1'), { scale: 1, opacity: 1, rotation: 0 });
 }
 
 function animateText() {
-        TweenMax.from($('h1'), 0.8, {
+    gsap.from($('h1'), {
+        duration: 0.8,
         scale: 0.4,
         opacity: 0,
         rotation: 15,
-        ease: Back.easeOut.config(4),
+        ease: "back.out(4)"
     });
 }
-    
-function animateBlobs() {
-    
-    var xSeed = _.random(350, 380);
-    var ySeed = _.random(120, 170);
-    
-    $.each($('.blob'), function(i) {
-        var $blob = $(this);
-        var speed = _.random(1, 5);
-        var rotation = _.random(5, 100);
-        var scale = _.random(0.8, 1.5);
-        var x = _.random(-xSeed, xSeed);
-        var y = _.random(-ySeed, ySeed);
 
-        TweenMax.to($blob, speed, {
+function animateBlobs() {
+    const xSeed = _.random(350, 380);
+    const ySeed = _.random(120, 170);
+
+    $('.blob').each(function() {
+        const $blob = $(this);
+        const speed = _.random(1, 5);
+        const rotation = _.random(5, 100);
+        const scale = _.random(0.8, 1.5);
+        const x = _.random(-xSeed, xSeed);
+        const y = _.random(-ySeed, ySeed);
+
+        gsap.to($blob, {
+            duration: speed,
             x: x,
             y: y,
-            ease: Power1.easeOut,
             opacity: 0,
             rotation: rotation,
             scale: scale,
-            onStartParams: [$blob],
-            onStart: function($element) {
-                $element.css('display', 'block');
+            ease: "power1.out",
+            onStart: function() {
+                $blob.css('display', 'block');
             },
-            onCompleteParams: [$blob],
-            onComplete: function($element) {
-                $element.css('display', 'none');
+            onComplete: function() {
+                $blob.css('display', 'none');
             }
         });
     });
