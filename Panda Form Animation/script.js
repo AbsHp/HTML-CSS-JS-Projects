10302db7d0c3414b0331b35651946db3e5c88e16
AbsHp <asinghal21@iitk.ac.in>
2024-11-30 23:47:47 +0530

    c14

---
+++ Panda Form Animation/script.js
@@ -0,0 +1,7 @@
+let usernameRef = document.getElementById("username");
+let passwordRef = document.getElementById("password");
+let eyeL = document.querySelector(".eyeball-l");
+let eyeR = document.querySelector(".eyeball-r");
+let handL = document.querySelector(".hand-l");
+let handR = document.querySelector(".hand-r");
+
