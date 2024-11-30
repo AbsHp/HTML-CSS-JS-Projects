9ed0b5cfef60393d653bf233617ef4aed7334e86
AbsHp <asinghal21@iitk.ac.in>
2024-12-01 01:15:29 +0530

    formatting

--- Panda Form Animation/script.js
+++ Panda Form Animation/script.js
@@ -6,60 +6,60 @@ let handL = document.querySelector(".hand-l");
 let handR = document.querySelector(".hand-r");
 
 let normalEyeStyle = () => {
-    eyeL.style.cssText = `
+  eyeL.style.cssText = `
       left:0.6em;
       top: 0.6em;
     `;
-    eyeR.style.cssText = `
+  eyeR.style.cssText = `
     right:0.6em;
     top:0.6em;
     `;
-  };
-  
-  let normalHandStyle = () => {
-    handL.style.cssText = `
+};
+
+let normalHandStyle = () => {
+  handL.style.cssText = `
           height: 2.81em;
           top:8.4em;
           left:7.5em;
           transform: rotate(0deg);
       `;
-    handR.style.cssText = `
+  handR.style.cssText = `
           height: 2.81em;
           top: 8.4em;
           right: 7.5em;
           transform: rotate(0deg)
       `;
-  };
-  usernameRef.addEventListener("focus", () => {
-    eyeL.style.cssText = `
+};
+usernameRef.addEventListener("focus", () => {
+  eyeL.style.cssText = `
       left: 0.75em;
       top: 1.12em;  
     `;
-    eyeR.style.cssText = `
+  eyeR.style.cssText = `
       right: 0.75em;
       top: 1.12em;
     `;
-    normalHandStyle();
-  });
-  passwordRef.addEventListener("focus", () => {
-    handL.style.cssText = `
+  normalHandStyle();
+});
+passwordRef.addEventListener("focus", () => {
+  handL.style.cssText = `
           height: 6.56em;
           top: 3.87em;
           left: 11.75em;
           transform: rotate(-155deg);    
       `;
-    handR.style.cssText = `
+  handR.style.cssText = `
       height: 6.56em;
       top: 3.87em;
       right: 11.75em;
       transform: rotate(155deg);
     `;
+  normalEyeStyle();
+});
+document.addEventListener("click", (e) => {
+  let clickedElem = e.target;
+  if (clickedElem != usernameRef && clickedElem != passwordRef) {
     normalEyeStyle();
-  });
-  document.addEventListener("click", (e) => {
-    let clickedElem = e.target;
-    if (clickedElem != usernameRef && clickedElem != passwordRef) {
-      normalEyeStyle();
-      normalHandStyle();
-    }
-  });
+    normalHandStyle();
+  }
+});
