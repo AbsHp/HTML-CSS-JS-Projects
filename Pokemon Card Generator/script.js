7254f8eaa89a1133aae277af35b9df00d1fe7c55
AbsHp <asinghal21@iitk.ac.in>
2024-12-04 23:52:23 +0530

    c12

--- Pokemon Card Generator/script.js
+++ Pokemon Card Generator/script.js
@@ -33,4 +33,16 @@ const typeColor = {
       });
   };
   
+  //Generate Card
   
+  let generateCard = (data) => {
+    // Get necessary data and assign it to variables
+    console.log(data);
+    const hp = data.stats[0].base_stat;
+    const imgSrc = data.sprites.other.dream_world.front_default;
+    const pokeName = data.name[0].toUpperCase() + data.name.slice(1);
+    const statAttack = data.stats[1].base_stat;
+    const statDefense = data.stats[2].base_stat;
+    const statSpeed = data.stats[5].base_stat;
+  
+    
