151f502e179c21f1082b6d5b370d2ae856cc1afd
AbsHp <asinghal21@iitk.ac.in>
2024-12-04 23:52:49 +0530

    c13

--- Pokemon Card Generator/script.js
+++ Pokemon Card Generator/script.js
@@ -45,4 +45,35 @@ const typeColor = {
     const statDefense = data.stats[2].base_stat;
     const statSpeed = data.stats[5].base_stat;
   
-    
+    // Set themeColor based on pokemon type
+    const themeColor = typeColor[data.types[0].type.name];
+    console.log(themeColor);
+    card.innerHTML = `
+          <p class="hp">
+            <span>HP</span>
+              ${hp}
+          </p>
+          <img src=${imgSrc} />
+          <h2 class="poke-name">${pokeName}</h2>
+          <div class="types">
+           
+          </div>
+          <div class="stats">
+            <div>
+              <h3>${statAttack}</h3>
+              <p>Attack</p>
+            </div>
+            <div>
+              <h3>${statDefense}</h3>
+              <p>Defense</p>
+            </div>
+            <div>
+              <h3>${statSpeed}</h3>
+              <p>Speed</p>
+            </div>
+          </div>
+    `;
+    appendTypes(data.types);
+    styleCard(themeColor);
+  };
+  
