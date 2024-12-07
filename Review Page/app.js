c529a10eda880b9219e90b11bfc9611b034c8390
AbsHp <asinghal21@iitk.ac.in>
2024-12-07 23:39:42 +0530

    c25

--- Review Page/app.js
+++ Review Page/app.js
@@ -7,4 +7,13 @@ if (data.length === 1) {
     nextBtn.style.display = "none";
     prevBtn.style.display = "none";
     }
+    // if length is 2, add copies of slides
+    let people = [...data];
+    if (data.length === 2) {
+    people = [...data, ...data];
+    }
+    container.innerHTML = people
+    .map((person, slideIndex) => {
+    const { img, name, job, text } = person;
+    let position = "next";
     
