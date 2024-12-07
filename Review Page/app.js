25cf25683d9d397b5727f84f37fade9c4c155808
AbsHp <asinghal21@iitk.ac.in>
2024-12-07 23:39:16 +0530

    c24

--- Review Page/app.js
+++ Review Page/app.js
@@ -2,3 +2,9 @@ import data from "./data.js";
 const container = document.querySelector(".slide-container");
 const nextBtn = document.querySelector(".next-btn");
 const prevBtn = document.querySelector(".prev-btn");
+// if length is 1 hide buttons
+if (data.length === 1) {
+    nextBtn.style.display = "none";
+    prevBtn.style.display = "none";
+    }
+    
