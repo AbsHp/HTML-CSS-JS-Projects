09fe1eda0c8a3d8dd60fa6ac71e924a972a61294
AbsHp <asinghal21@iitk.ac.in>
2024-12-08 00:06:00 +0530

    image errors fixed

--- Review Page/data.js
+++ Review Page/data.js
@@ -1,21 +1,21 @@
 const people = [
     {
         img:
-            "https://res.cloudinary.com/diqqf3eq2/image/upload/c_scale,w_200/v1595959121/person-1_aufeoq.jpg",
+            "https://drive.google.com/file/d/1gzboDKXhrmp2bnna-DQFcj6-vnpvzjof/view?usp=sharing",
         name: "Suryansh Gaur",
         job: "First Secretary, IFS",
         text: `Lorem ipsum dolor sit amet consectetur adipisicing elit. Quidem quoeius recusandae officia voluptas sint deserunt dicta nihil nam omnis? `,
     },
     {
         img:
-            "https://res.cloudinary.com/diqqf3eq2/image/upload/c_scale,w_200/v1595959131/person-2_ipcjws.jpg",
+            "https://drive.google.com/file/d/1uwPSKQm3UlrTCWWJy_OrFYkgLdBqzTwu/view?usp=sharing",
         name: "Shreya Rajak",
         job: "Distinguished Scientist, ISRO",
         text: `Lorem ipsum dolor sit amet consectetur adipisicing elit. Quidem quoeius recusandae officia voluptas sint deserunt dicta nihil nam omnis?`,
     },
     {
         img:
-            "https://res.cloudinary.com/diqqf3eq2/image/upload/c_scale,w_200/v1595959131/person-3_rxtqvi.jpg",
+            "https://drive.google.com/file/d/1ZvDQrwh7akx8BStn4q7bYxWrwf0uFZhC/view?usp=sharing",
         name: "Vaishnavi Singh",
         job: "Senior Product Manager, Microsoft India",
         text: `Lorem ipsum dolor sit amet consectetur adipisicing elit. Quidem quoeius recusandae officia voluptas sint deserunt dicta nihil nam omnis?`,
