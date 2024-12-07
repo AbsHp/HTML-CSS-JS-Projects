e1511b4b3ef8f13b55b292c61e31fd5fdf87e6c1
AbsHp <asinghal21@iitk.ac.in>
2024-12-07 23:49:50 +0530

    c32

--- Review Page/data.js
+++ Review Page/data.js
@@ -6,6 +6,13 @@ const people = [
     job: "First Secretary, IFS",
     text: `Lorem ipsum dolor sit amet consectetur adipisicing elit. Quidem quoeius recusandae officia voluptas sint deserunt dicta nihil nam omnis? `,
     },
-    
+    {
+        img:
+        "https://res.cloudinary.com/diqqf3eq2/image/upload/c_scale,w_200/v1595959131/person-2_ipcjws.jpg",
+        name: "Shreya Rajak",
+        job: "Distinguished Scientist, ISRO",
+        text: `Lorem ipsum dolor sit amet consectetur adipisicing elit. Quidem quoeius recusandae officia voluptas sint deserunt dicta nihil nam omnis?`,
+        },
+        
     ];
     export default people;
