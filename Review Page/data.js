b1d1f8bd6dd6907374827860eb40ac0ccd587542
AbsHp <asinghal21@iitk.ac.in>
2024-12-07 23:50:54 +0530

    c33

--- Review Page/data.js
+++ Review Page/data.js
@@ -13,6 +13,12 @@ const people = [
         job: "Distinguished Scientist, ISRO",
         text: `Lorem ipsum dolor sit amet consectetur adipisicing elit. Quidem quoeius recusandae officia voluptas sint deserunt dicta nihil nam omnis?`,
         },
-        
+        {
+            img:
+            "https://res.cloudinary.com/diqqf3eq2/image/upload/c_scale,w_200/v1595959131/person-3_rxtqvi.jpg",
+            name: "Vaishnavi Singh",
+            job: "Senior Product Manager, Microsoft India",
+            text: `Lorem ipsum dolor sit amet consectetur adipisicing elit. Quidem quoeius recusandae officia voluptas sint deserunt dicta nihil nam omnis?`,
+            },
     ];
     export default people;
