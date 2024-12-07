19c5cd066c89333b38affae770857118c25e72d6
AbsHp <asinghal21@iitk.ac.in>
2024-12-07 23:40:03 +0530

    c26

--- Review Page/app.js
+++ Review Page/app.js
@@ -16,4 +16,25 @@ if (data.length === 1) {
     .map((person, slideIndex) => {
     const { img, name, job, text } = person;
     let position = "next";
-    
+    if (slideIndex === 0) {
+        position = "active";
+        }
+        if (slideIndex === people.length - 1) {
+        position = "last";
+        }
+        if (data.length <= 1) {
+        position = "active";
+        }
+        return `<article class="slide ${position}">
+        <img src=${img} class="img" alt="${name}"/>
+        <h4>${name}</h4>
+        <p class="title">${job}</p>
+        <p class="text">
+        ${text}
+        </p>
+        <div class="quote-icon">
+        <i class="fas fa-quote-right"></i>
+        </div>
+        </article>`;
+        })
+        
