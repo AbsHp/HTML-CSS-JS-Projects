89cc624acf88cb9e6530eb63f0352343dddbcb4d
AbsHp <asinghal21@iitk.ac.in>
2024-12-07 23:46:00 +0530

    c31

---
+++ Review Page/data.js
@@ -0,0 +1,11 @@
+const people = [
+    {
+    img:
+    "https://res.cloudinary.com/diqqf3eq2/image/upload/c_scale,w_200/v1595959121/person-1_aufeoq.jpg",
+    name: "Suryansh Gaur",
+    job: "First Secretary, IFS",
+    text: `Lorem ipsum dolor sit amet consectetur adipisicing elit. Quidem quoeius recusandae officia voluptas sint deserunt dicta nihil nam omnis? `,
+    },
+    
+    ];
+    export default people;
