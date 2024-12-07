cdfd129a76a5cb40b6f7fb92a5ba1f2bf91a1f80
AbsHp <asinghal21@iitk.ac.in>
2024-12-07 23:51:08 +0530

    minor cleanup

--- Review Page/data.js
+++ Review Page/data.js
@@ -1,24 +1,24 @@
 const people = [
     {
-    img:
-    "https://res.cloudinary.com/diqqf3eq2/image/upload/c_scale,w_200/v1595959121/person-1_aufeoq.jpg",
-    name: "Suryansh Gaur",
-    job: "First Secretary, IFS",
-    text: `Lorem ipsum dolor sit amet consectetur adipisicing elit. Quidem quoeius recusandae officia voluptas sint deserunt dicta nihil nam omnis? `,
+        img:
+            "https://res.cloudinary.com/diqqf3eq2/image/upload/c_scale,w_200/v1595959121/person-1_aufeoq.jpg",
+        name: "Suryansh Gaur",
+        job: "First Secretary, IFS",
+        text: `Lorem ipsum dolor sit amet consectetur adipisicing elit. Quidem quoeius recusandae officia voluptas sint deserunt dicta nihil nam omnis? `,
     },
     {
         img:
-        "https://res.cloudinary.com/diqqf3eq2/image/upload/c_scale,w_200/v1595959131/person-2_ipcjws.jpg",
+            "https://res.cloudinary.com/diqqf3eq2/image/upload/c_scale,w_200/v1595959131/person-2_ipcjws.jpg",
         name: "Shreya Rajak",
         job: "Distinguished Scientist, ISRO",
         text: `Lorem ipsum dolor sit amet consectetur adipisicing elit. Quidem quoeius recusandae officia voluptas sint deserunt dicta nihil nam omnis?`,
-        },
-        {
-            img:
+    },
+    {
+        img:
             "https://res.cloudinary.com/diqqf3eq2/image/upload/c_scale,w_200/v1595959131/person-3_rxtqvi.jpg",
-            name: "Vaishnavi Singh",
-            job: "Senior Product Manager, Microsoft India",
-            text: `Lorem ipsum dolor sit amet consectetur adipisicing elit. Quidem quoeius recusandae officia voluptas sint deserunt dicta nihil nam omnis?`,
-            },
-    ];
-    export default people;
+        name: "Vaishnavi Singh",
+        job: "Senior Product Manager, Microsoft India",
+        text: `Lorem ipsum dolor sit amet consectetur adipisicing elit. Quidem quoeius recusandae officia voluptas sint deserunt dicta nihil nam omnis?`,
+    },
+];
+export default people;
