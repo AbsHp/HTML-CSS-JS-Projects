91aeb7b72902920a4ed1a5a943944b313f13ad0b
AbsHp <asinghal21@iitk.ac.in>
2024-12-07 23:38:48 +0530

    c22

---
+++ Review Page/app.js
@@ -0,0 +1,4 @@
+import data from "./data.js";
+const container = document.querySelector(".slide-container");
+const nextBtn = document.querySelector(".next-btn");
+const prevBtn = document.querySelector(".prev-btn");
