f350d4d63ebd11f9840cb3a1447d7db488ffc50b
AbsHp <asinghal21@iitk.ac.in>
2025-01-01 23:42:00 +0530

    c18

--- Parallax Tilt Card/script.js
+++ Parallax Tilt Card/script.js
@@ -6,4 +6,20 @@ class parallaxTiltEffect {
     [this.w, this.h] = this.size;
     this.tiltEffect = tiltEffect;
     this.mouseOnComponent = false;
+    this.handleMouseMove = this.handleMouseMove.bind(this);
+    this.handleMouseEnter = this.handleMouseEnter.bind(this);
+    this.handleMouseLeave = this.handleMouseLeave.bind(this);
+    this.defaultStates = this.defaultStates.bind(this);
+    this.setProperty = this.setProperty.bind(this);
+    this.init = this.init.bind(this);
+    this.init();
+    }
+    handleMouseMove(event) {
+    const {offsetX, offsetY} = event;
+    let X;
+    let Y;
+    if (this.tiltEffect === "reverse") {
+    X = ((offsetX - (this.w/2)) / 3) / 3;
+    Y = (-(offsetY - (this.h/2)) / 3) / 3;
+    }
     
